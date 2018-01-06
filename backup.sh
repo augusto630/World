a02c75902c198f21485d3e2a4923ed3ca0123323
root <root@vps164669.vps.ovh.ca>
2018-01-06 15:35:25 +0000

    Auto-Backup 8

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 #59 20   * * *   root    /home/minecraft/World/backup.sh
 
 echo "Parando Minecraft"
-systemctl stop minecraft.service
+systemctl stop minecraft.service >/home/minecraft/backup.log 2>&1
 sleep 5
 
 echo "Iniciando Backup"
@@ -12,12 +12,12 @@ COMMIT_COUNT=`git rev-list HEAD --count`
 COMMIT_COUNT=$(($COMMIT_COUNT+1))
 
 echo "Adicionando novos arquivos"
-git add .
-git commit -m "Auto-Backup $COMMIT_COUNT"
+git add . >/home/minecraft/backup.log 2>&1
+git commit -m "Auto-Backup $COMMIT_COUNT" >/home/minecraft/backup.log 2>&1
 
 echo "Enviando..."
-git push -u origin master
+git push -u origin master >/home/minecraft/backup.log 2>&1
 
 #echo "Iniciando Minecraft"
-systemctl start minecraft.service
+systemctl start minecraft.service >/home/minecraft/backup.log 2>&1
 echo "Finalizado"
